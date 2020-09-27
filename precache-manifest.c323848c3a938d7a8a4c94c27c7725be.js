79db6a77f1ffd8dbda3e1da23d3c01a0f0c62e58
Elena Maximova <pumpkinthestrange@gmail.com>
2020-09-27 22:09:49 +0300

    Updates

---
+++ precache-manifest.c323848c3a938d7a8a4c94c27c7725be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735b1d67ec0616a32eb3b4dcc8ddb8bb",
+    "url": "/AndreyMelnikovPhoto/index.html"
+  },
+  {
+    "revision": "28a8dd46dc35af4cd5e2",
+    "url": "/AndreyMelnikovPhoto/static/css/main.aec4796d.chunk.css"
+  },
+  {
+    "revision": "d76e9e39df17150bd46f",
+    "url": "/AndreyMelnikovPhoto/static/js/2.25a23ce6.chunk.js"
+  },
+  {
+    "revision": "85c1ecd325742a4c0551d2653970c6fb",
+    "url": "/AndreyMelnikovPhoto/static/js/2.25a23ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28a8dd46dc35af4cd5e2",
+    "url": "/AndreyMelnikovPhoto/static/js/main.4a36cce3.chunk.js"
+  },
+  {
+    "revision": "9e568650adf0d9044884",
+    "url": "/AndreyMelnikovPhoto/static/js/runtime-main.69a35708.js"
+  }
+]);
