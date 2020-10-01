0f2d07f634ff2bcf8a3adeff80a5412cdb1251cd
System Administrator <pumpkinthestrange@gmail.com>
2020-10-01 18:56:13 +0300

    fix aboutpage

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,9 @@ import React from 'react'
 function About() {
     return(
         <div className="about"> 
-            
+            <h3 id="about-title">О Дике Членсе</h3>
             <div className="aboutpic">
-                <h3 id="about-title">О Дике Членсе</h3>
+                
                 <img id="mypic"
                     src="https://andreyphotos.s3.eu-north-1.amazonaws.com/about+photo/IMG_7448w300.jpg"
                     srcSet="
@@ -16,8 +16,8 @@ function About() {
                     (max-width: 1200px) and (min-width: 600px) 600px,
                     (max-width: 470px) 300px, "
                 />
-
-    
+            </div>
+            <div>
                 <p className="about-text">Полицейский пристав Ричард Членс получает очередное задание, целью которого является ни много ни мало — спасение всей Земли. Дело в том, что его начальство подозревает, что на данный момент в кресле президента сидит не он сам, а его клон, и чтобы прозондировать почву относительного этого вопроса, засылает Членса на Луну. Судя по всему, именно там и базируется эта самая клонирующая фабрика, намеревающаяся в скором времени заполонить Землю клонами известных личностей.</p>
                 <p className="about-text"></p>
             </div>
