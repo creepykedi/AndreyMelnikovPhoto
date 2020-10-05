eb3b133eaed1258225b3bbfbc84bf561ab804f36
System Administrator <pumpkinthestrange@gmail.com>
2020-10-05 22:56:43 +0300

    change to webp

--- src/components/Rep.js
+++ src/components/Rep.js
@@ -8,13 +8,13 @@ function Rep() {
         <div>
             <div className="pic" >
            
-    
+    <SRLWrapper>
                 
-                <img srcset="https://storage.yandexcloud.net/am-photo/architecture/DJI_0014.jpg 300w,
-                https://moodboardbucket.s3.amazonaws.com/static/moodboard/images/q1n9f3x3k5d31_ombAZuZ.jpg 600w,
-                https://preview.redd.it/aqa1lhk2ybk51.jpg?width=640&crop=smart&auto=webp&s=60bb247123c49e99f891072f3a7960b490bd6973 900w" alt="main"/>
+                <img srcset="https://storage.yandexcloud.net/am-photo/DJI_0014.webp 1200w,
+                https://moodboardbucket.s3.amazonaws.com/static/moodboard/images/q1n9f3x3k5d31_ombAZuZ.jpg 750w,
+                https://preview.redd.it/aqa1lhk2ybk51.jpg?width=640&crop=smart&auto=webp&s=60bb247123c49e99f891072f3a7960b490bd6973 1200w" alt="main"/>
             
-            
+            </SRLWrapper>
             </div>
         </div>
 
